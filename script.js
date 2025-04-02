8b2da881f2939940a1e84bd556679140841e26ff
Aryan Devanand Shinde <132800298+feENGINEERINGnotes@users.noreply.github.com>
2025-04-02 18:49:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+const options={
+    bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+// let count = 0;
+// function thr()
+// {
+//     count++;
+//     if(count%2==0)
+//     {
+//         document.querySelector(".container-fluid").style.backgroundColor = "rgb(255, 255, 255)";
+//     }
+//     else{
+//         document.querySelector(".container-fluid").style.backgroundColor = "rgba(112, 112, 112, 0.936)"; 
+//     }
+// }
+    
